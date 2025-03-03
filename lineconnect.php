09e6345a668229e170e9dcc8fc2908112a7f45b3
ship <shipwebdotjp@gmail.com>
2025-03-03 23:22:30 +0900

    ### Added - リッチメニューとリッチメニューエイリアスの作成、管理機能を追加 - 個別ユーザーとリッチメニューとのリンクが行えるアクションを追加 - Added ability to create and manage rich menus and rich menu aliases - Added an action to link individual users to the rich menu

--- lineconnect.php
+++ lineconnect.php
@@ -4,7 +4,7 @@
 	Plugin Name: LINE Connect
 	Plugin URI: https://blog.shipweb.jp/lineconnect/
 	Description: Account link between WordPress user ID and LINE ID
-	Version: 3.4.0
+	Version: 4.0.0
 	Author: shipweb
 	Author URI: https://blog.shipweb.jp/about
 	License: GPLv3
