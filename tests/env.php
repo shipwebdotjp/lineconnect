422521fd64223e0fd14e4fd6f12f1c136119c6a1
ship <shipwebdotjp@gmail.com>
2025-09-02 21:55:14 +0900

    add debug

--- tests/env.php
+++ tests/env.php
@@ -4,5 +4,9 @@ if (!getenv('WP_TESTS_DIR')) {
     error_log('Loading .env file for tests');
     $dotenv->load();
     error_log($_ENV['WP_TESTS_DIR']);
+    $_tests_dir = $_ENV['WP_TESTS_DIR'];
+} else {
+    error_log('Using existing env var for tests');
+    $_tests_dir = getenv('WP_TESTS_DIR');
+    error_log($_tests_dir);
 }
-$_tests_dir = $_ENV['WP_TESTS_DIR'];
