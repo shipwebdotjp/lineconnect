8dc5adb18c47527ea513611eb85e2e0eb9f3c16d
ship <shipwebdotjp@gmail.com>
2025-09-02 16:26:46 +0900

    get env on dev

--- tests/env.php
+++ tests/env.php
@@ -1,4 +1,6 @@
 <?php
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-$_tests_dir = $_ENV['WP_TESTS_DIR'];
+if (!getenv('WP_TESTS_DIR')) {
+    $dotenv->load();
+}
+$_tests_dir = getenv('WP_TESTS_DIR');
